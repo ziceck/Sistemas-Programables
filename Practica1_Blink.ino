69aac9c5d2eb18101c3bd30f7c3b11ace0479693
Ziceck <frikyequisde@live.com>
2017-07-08 21:42:37 -0500

    add comments

--- Practica1_Blink.ino
+++ Practica1_Blink.ino
@@ -4,10 +4,12 @@ Este ejemplo hace parpadear un led.
 
 
 void setup() {
+  //Se configura el pin 13 cómo pin de salida.
   pinMode(13, OUTPUT);
 }
 
 void loop() {
+  //En un pin de salida se puede escribir un valor.
   digitalWrite(13, HIGH);
   delay(1000);                  
   digitalWrite(13, LOW);  
