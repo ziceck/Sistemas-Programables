9cc83e7da009cbe1da89eee92ad7dec2ceee172b
Ziceck <frikyequisde@live.com>
2017-07-08 21:45:13 -0500

    Add files via upload

---
+++ Practica3_Fotoresistencia.ino
@@ -0,0 +1,20 @@
+int sensorPin = A0;
+int ledPin = 13;
+int sensorValue = 0;
+
+void setup() {
+  pinMode(ledPin,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue=analogRead(sensorPin);
+  Serial.println(sensorValue);
+  if(sensorValue<=500){
+    digitalWrite(ledPin,HIGH);
+  }
+  else{
+    digitalWrite(ledPin,LOW);
+  }
+  delay(10);
+}
