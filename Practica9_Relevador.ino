be6cd8dbb4e3727b22f3cbf53b35e1c38b829acd
Ziceck <frikyequisde@live.com>
2017-07-09 18:27:13 -0500

    Add files via upload

--- Practica9_Relevador.ino
+++ Practica9_Relevador.ino
@@ -1,9 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
-
+int ledPin=13;
+void setup()
+{
+  pinMode(ledPin,OUTPUT);
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  digitalWrite(ledPin,HIGH);
+  delay(1000);
+  digitalWrite(ledPin,LOW);
+  delay(1000);
 }
+
+  
