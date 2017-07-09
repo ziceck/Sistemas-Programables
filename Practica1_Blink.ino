5e37a8cc06eaf3dd98c8fcb961150ee5ba72af0e
Ziceck <frikyequisde@live.com>
2017-07-08 21:41:36 -0500

    Add files via upload

---
+++ Practica1_Blink.ino
@@ -0,0 +1,15 @@
+/*
+Este ejemplo hace parpadear un led.
+*/
+
+
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(1000);                  
+  digitalWrite(13, LOW);  
+  delay(1000);
+}
