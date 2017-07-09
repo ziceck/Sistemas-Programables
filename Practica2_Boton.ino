bd9bc4818d56d75c7167b2fdcc386179b57f7b38
Ziceck <frikyequisde@live.com>
2017-07-08 21:43:31 -0500

    Add files via upload

---
+++ Practica2_Boton.ino
@@ -0,0 +1,16 @@
+/***
+ * 
+ * Este codigo enciende un led usando un push boton
+ */
+
+void setup(){
+  //se leen los pines configurados cómo INPUT
+  pinMode(8,INPUT);
+  // se escribe en los pines configurados con OUTPUT
+  pinMode(13,OUTPUT);
+}
+
+void loop(){
+  digitalWrite(13,digitalRead(8));
+}
+
