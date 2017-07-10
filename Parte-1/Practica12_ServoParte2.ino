d090653893f3d6fdf8bf1a84e61d787f600a5566
Ziceck <frikyequisde@live.com>
2017-07-10 17:04:46 -0500

    Add files via upload

---
+++ Parte-1/Practica12_ServoParte2.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int potpin=0;
+int val;
+
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val=analogRead(potpin);
+  val=map(val,0,1023,0,179);
+  myservo.write(val);
+  delay(15);
+}
