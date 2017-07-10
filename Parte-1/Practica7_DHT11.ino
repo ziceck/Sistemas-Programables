6b6167304dbd10d030483399b713224f32df450c
Ziceck <frikyequisde@live.com>
2017-07-10 17:02:25 -0500

    Rename Practica7_DHT11.ino to Parte-1/Practica7_DHT11.ino

---
+++ Parte-1/Practica7_DHT11.ino
@@ -0,0 +1,32 @@
+/*
+ * Este código sirve para probar el sensor DHT11.
+ */
+#include <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN,DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  dht.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float h=dht.readHumidity();
+  float t=dht.readTemperature();
+
+  if(isnan(t)||isnan(h)){
+    Serial.println("Failed to read from DHT");
+  }
+  else{
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.println( "*C");
+  }
+}
