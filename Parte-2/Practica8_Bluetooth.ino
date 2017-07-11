5324a9525ea06357b0a96f4c9ec6f698c1247956
Ziceck <frikyequisde@live.com>
2017-07-10 21:04:39 -0500

    Add files via upload

---
+++ Parte-2/Practica8_Bluetooth.ino
@@ -0,0 +1,32 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(4,2);
+
+int a=0;
+char c;
+char d;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Buenas noches Luna!!");
+   
+  mySerial.begin(9600);
+  mySerial.println("Hola, Mundo");
+}
+
+void loop() {
+  delay(10);
+
+  if(Serial.available()){
+    c=Serial.read();
+    delay(10);
+    mySerial.write(c);
+    }
+  delay(10);
+  
+  if(mySerial.available()){
+    d=mySerial.read();
+    delay(10);
+    Serial.write(d);    
+    }
+}
