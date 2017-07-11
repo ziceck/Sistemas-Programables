0a6349fb25e0370c698859aa24cf62cbfdeffcc0
Ziceck <frikyequisde@live.com>
2017-07-10 20:59:16 -0500

    Add files via upload

---
+++ Parte-2/Practica2_RainSensor.ino
@@ -0,0 +1,24 @@
+const int sensorMin=0;
+const int sensorMax=1024;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorReading=analogRead(A0);
+  int range=map(sensorReading,sensorMin,sensorMax,0,3);
+
+  switch(range){
+    case 0:
+      Serial.println("Lluvia");
+      break;
+    case 1:
+      Serial.println("Riesgo De Lluvia");
+      break;
+    case 2:
+      Serial.println("Sin Lluvia");
+      break;
+  }
+  delay(1);
+}
