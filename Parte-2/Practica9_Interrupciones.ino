765cffa51542af3266482e023cef1f6298bdc95d
Ziceck <frikyequisde@live.com>
2017-07-10 21:04:59 -0500

    Add files via upload

---
+++ Parte-2/Practica9_Interrupciones.ino
@@ -0,0 +1,32 @@
+long start = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(2, INPUT);
+
+  digitalWrite(2, HIGH);
+
+  randomSeed(millis());
+ 
+  delay(random(1000, 3000));
+
+  digitalWrite(13, HIGH);
+
+  attachInterrupt(0, react, FALLING);
+
+  start = millis();
+}
+
+void loop() {
+
+}
+
+void react(){
+  long fin = millis();
+  
+  detachInterrupt(0);
+  
+  Serial.print("Tiempo: ");
+  Serial.println(fin - start);
+  }
