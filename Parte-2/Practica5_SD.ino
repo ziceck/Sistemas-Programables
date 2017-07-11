b18915032a5731046bda091b39ebc43c0ce687d6
Ziceck <frikyequisde@live.com>
2017-07-10 21:00:24 -0500

    Add files via upload

---
+++ Parte-2/Practica5_SD.ino
@@ -0,0 +1,40 @@
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+  }
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(4)) {
+  Serial.println("initialization failed!");
+  return;
+  }
+  Serial.println("initialization done.");
+  myFile = SD.open("test.txt", FILE_WRITE);
+  if (myFile) {
+  Serial.print("Writing to test.txt...");
+  myFile.println("testing 1, 2, 3.");
+  myFile.close();
+  Serial.println("done.");
+  } else {
+  Serial.println("error opening test.txt");
+  }
+  myFile = SD.open("test.txt");
+  if (myFile) {
+  Serial.println("test.txt:");
+
+
+  while (myFile.available()) {
+    Serial.write(myFile.read());
+  }
+  myFile.close();
+  } else {
+  Serial.println("error opening test.txt");
+  }
+}
+
+void loop() {
+
+}
