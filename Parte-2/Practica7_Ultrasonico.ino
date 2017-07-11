9c4e233ed7ee1a4e08804a5d1c91185bff1a2b9b
Ziceck <frikyequisde@live.com>
2017-07-10 21:04:18 -0500

    Add files via upload

---
+++ Parte-2/Practica7_Ultrasonico.ino
@@ -0,0 +1,28 @@
+int trigPin = 8;
+int echoPin = 7;
+float v = 331.5 + 0.6 * 20;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+float distanceM(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(3);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(5);  
+  digitalWrite(trigPin, LOW);
+
+  float tUs = pulseIn(echoPin, HIGH);
+  float t = tUs / 1000.0 / 1000.0 / 2;
+  float d = t*v;
+  return d*100;
+  }
+
+void loop() {
+  int d = distanceM();
+  Serial.println(d, DEC);
+  delay(200);
+}
